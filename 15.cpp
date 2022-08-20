9a75be65ad0c5371c3c455abacac0c0b10b52350
Miao <andyb0127118@gmail.com>
2022-08-20 07:11:07 +0000

    Update 15.cpp

--- 15.cpp
+++ 15.cpp
@@ -2,6 +2,36 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
+        int tar = 0, front = 0, end = 0, sum = 0;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size(); ++i){
+            tar = -nums[i];
+            front = i+1;
+            end = nums.size()-1;
+            while(front<end){
+                sum = nums[front] + nums[end];
+                if(sum < tar) front++;
+                else if(sum > tar) end--;
+                else{
+                    vector<int> tmp = {-tar, nums[front], nums[end]};
+                    res.push_back(tmp);
+                    while(front < end && nums[front]==tmp[1]) front++;
+                    while(front < end && nums[end]==tmp[2]) end--;
+                }
+            }
+            while(((i+1) < nums.size()) && nums[i] == nums[i+1]){
+                ++i;
+            }
+        }
+        return res;
+    }
+};
+
+/*
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
         int tar = 0;
         int l = 0, r = 0;
         sort(nums.begin(), nums.end());
@@ -27,3 +57,4 @@ public:
         return res;
     }
 };
+*/
