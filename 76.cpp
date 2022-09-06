ef14bf166370c93d5d609c59c8ea74f4ca8599e8
Miao <andyb0127118@gmail.com>
2022-09-06 17:01:03 +0000

    Update 76.cpp

--- 76.cpp
+++ 76.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        //Input: s = "ADOBECODEBANC", t = "ABC"
+        //Output: "BANC"
+        int p_fast = 0, p_slow = 0, start = 0;
+        int tar_len = t.size(), min = INT_MAX;
+        vector<int> vec(128,0);
+        for(auto c:t) {vec[c]++;}
+        while(p_slow < s.size() && p_fast <= s.size()){
+            if(tar_len > 0){
+                if(p_fast == s.size()) break;
+                if(--vec[s[p_fast++]] >= 0){
+                   tar_len--;
+                }  
+            }else{
+                if((p_fast - p_slow) < min){
+                    min = p_fast-p_slow;
+                    start = p_slow;
+                }
+                if(++vec[s[p_slow++]] > 0){
+                    tar_len++;
+                }
+            }
+        }
+       return min == INT_MAX ? "" : s.substr(start, min);
+    }
+};
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        //Input: s = "ADOBECODEBANC", t = "ABC"
+        //Output: "BANC"
         int start = 0, end = 0, cntr = t.size();
         int minStart = 0, minLen = INT_MAX;
         vector<int> dic(128, 0);
