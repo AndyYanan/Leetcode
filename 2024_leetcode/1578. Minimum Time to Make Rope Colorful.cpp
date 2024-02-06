feeb6cd6ea4791bf29667c6faa0d56b0ad02bc57
Miao <andyb0127118@gmail.com>
2024-02-07 01:27:38 +0800

    2024.0207

--- 2024_leetcode/1578. Minimum Time to Make Rope Colorful.cpp
+++ 2024_leetcode/1578. Minimum Time to Make Rope Colorful.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        
+        int res = 0;
+        int sum_tmp = neededTime[0], max_tmp = neededTime[0];
+        for(int i = 1; i < colors.size(); i++){
+            if(colors[i] != colors[i-1]){
+                res += sum_tmp - max_tmp;
+                sum_tmp = 0;
+                max_tmp = 0;
+            }
+            sum_tmp += neededTime[i];
+            max_tmp = max(max_tmp, neededTime[i]);
+        }
+        res += sum_tmp - max_tmp;
+        return res;
     }
 };
